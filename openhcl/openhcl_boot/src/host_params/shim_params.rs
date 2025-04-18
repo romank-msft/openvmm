fcf95df945b1e19424e21019db85f2331288b65f
Roman Kisel <romank@linux.microsoft.com>
2025-04-18 08:29:59 -0700

    Revert "shim scratch page"
    This reverts commit 6d7b5a5dd8efc7346656397fcaf0c1f693192121.

--- openhcl/openhcl_boot/src/host_params/shim_params.rs
+++ openhcl/openhcl_boot/src/host_params/shim_params.rs
@@ -107,9 +107,6 @@ pub struct ShimParams {
     /// Memory used by the shim.
     pub used: MemoryRange,
     pub bounce_buffer: Option<MemoryRange>,
-    /// Scratch page for re-entries. Cannot rely on the static data as
-    /// BSS is zeroed out.
-    pub scratch_page_addr: u64,
     /// Enable secure AVIC if supported.
     pub auto_enable_secure_avic: bool,
 }
@@ -138,7 +135,6 @@ impl ShimParams {
             used_end,
             bounce_buffer_start,
             bounce_buffer_size,
-            scratch_page_offset,
             auto_enable_secure_avic,
         } = raw;
 
@@ -173,7 +169,6 @@ impl ShimParams {
                     ..shim_base_address.wrapping_add_signed(used_end),
             ),
             bounce_buffer,
-            scratch_page_addr: shim_base_address.wrapping_add_signed(scratch_page_offset),
             auto_enable_secure_avic: auto_enable_secure_avic != 0,
         }
     }
