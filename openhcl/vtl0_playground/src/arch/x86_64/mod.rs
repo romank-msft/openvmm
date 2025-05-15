2fc1d586dc21684d26dd8e333b3cabc888be101c
Roman Kisel <romank@microsoft.com>
2025-05-15 16:01:38 -0700

    vtl0 playground

---
+++ openhcl/vtl0_playground/src/arch/x86_64/mod.rs
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#![cfg(target_arch = "x86_64")]
+
+//! x86_64 architecture-specific implementations.
+
+pub mod com1;
+
+// Entry point.
+#[cfg(minimal_rt)]
+core::arch::global_asm! {
+    include_str!("entry.S"),
+    relocate = sym minimal_rt::reloc::relocate,
+    start = sym crate::rt::start,
+    stack = sym crate::rt::STACK,
+    STACK_COOKIE = const crate::rt::STACK_COOKIE,
+    STACK_SIZE = const crate::rt::STACK_SIZE,
+}
